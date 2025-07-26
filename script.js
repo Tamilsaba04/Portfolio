ead35939ac2d0dc2a8954114829af48bd37271c9
Tamilselvan Sabapathy <157901445+Tamilsaba04@users.noreply.github.com>
2025-07-26 20:48:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute("href"));
+    section?.scrollIntoView({ behavior: "smooth" });
+  });
+});
